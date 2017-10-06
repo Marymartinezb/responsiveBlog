9a8289aca130581a0c311da13a12113cf53235b1
Mary Martinez <mary.angmart@gmail.com>
2017-10-06 11:56:53 -0600

    Fisrt commit on blog

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById('mainMenu');
+var imgMenu = document.getElementById('imgMenu');
+
+imgMenu.addEventListener("click", function(){
+    menu.classList.toggle('hidden');
+});
